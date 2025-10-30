c904a50805e90ace3041496d9ff958ad98c89873
Christian Neres Severino Martins Milfont <67718947+christianmilfont@users.noreply.github.com>
2025-10-30 10:57:44 -0300

    Change default MySQL host to a specific IP address

--- infra-app.sh
+++ infra-app.sh
@@ -2,9 +2,9 @@
 set -euo pipefail
 
 # ==========================
-# Par‚metros da Conex„o MySQL
+# Par√¢metros da Conex√£o MySQL
 # ==========================
-DbHost="${1:-localhost}"
+DbHost="${1:-20.241.181.58:3306}"
 DbPort="${2:-3306}"
 DbName="${3:-ProjetoAutorLivroDb}"
 DbUser="${4:-root}"
@@ -14,11 +14,11 @@ DbPass="${5:-senha123}"
 # Atividades do MySQL
 # =======================
 if ! command -v mysql >/dev/null 2>&1; then
-  echo "ERRO: cliente 'mysql' n„o encontrado. Instale-o antes de executar este script."
+  echo "ERRO: cliente 'mysql' n√£o encontrado. Instale-o antes de executar este script."
   exit 1
 fi
 
-echo "Criando banco de dados se n„o existir..."
+echo "Criando banco de dados se n√£o existir..."
 mysql -h"$DbHost" -P"$DbPort" -u"$DbUser" -p"$DbPass" -e "CREATE DATABASE IF NOT EXISTS \`$DbName\`;"
 
 echo "Criando Script de Banco..."
@@ -59,10 +59,10 @@ sku="F1"
 echo "Criando Grupo de Recursos..."
 az group create --name "$rg" --location "$location" 1>/dev/null
 
-echo "Criando Plano de ServiÁo..."
+echo "Criando Plano de Servi√ßo..."
 az appservice plan create --name "$plan" --resource-group "$rg" --location "$location" --sku "$sku" 1>/dev/null
 
-echo "Criando ServiÁo de Aplicativo..."
+echo "Criando Servi√ßo de Aplicativo..."
 az webapp create --resource-group "$rg" --plan "$plan" --runtime "$runtime" --name "$app" 1>/dev/null
 
 # Captura estado atual dos logs
@@ -71,9 +71,9 @@ ws_logging="$(az webapp log show -g "$rg" -n "$app" --query 'httpLogs.fileSystem
 det_errors="$(az webapp log show -g "$rg" -n "$app" --query 'detailedErrorMessages.enabled' -o tsv 2>/dev/null || true)"
 failed_req="$(az webapp log show -g "$rg" -n "$app" --query 'failedRequestsTracing.enabled' -o tsv 2>/dev/null || true)"
 
-# CondiÁ„o: aplicar somente se algo n„o estiver habilitado como desejado
+# Condi√ß√£o: aplicar somente se algo n√£o estiver habilitado como desejado
 if [ "$app_logging" != "Information" ] || [ "$ws_logging" != "true" ] || [ "$det_errors" != "true" ] || [ "$failed_req" != "true" ]; then
-  echo "Habilitando Logs do ServiÁo de Aplicativo..."
+  echo "Habilitando Logs do Servi√ßo de Aplicativo..."
   az webapp log config \
     --resource-group "$rg" \
     --name "$app" \
@@ -83,7 +83,7 @@ if [ "$app_logging" != "Information" ] || [ "$ws_logging" != "true" ] || [ "$det
     --detailed-error-messages true \
     --failed-request-tracing true 1>/dev/null
 else
-  echo "Logs j· configurados"
+  echo "Logs j√° configurados"
 fi
 
 echo "[OK] Objetos da Azure prontos"
