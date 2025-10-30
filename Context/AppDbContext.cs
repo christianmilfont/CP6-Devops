2d534256392fef6bfa1fa74684b171d8578ebe4e
Christian Neres Severino Martins Milfont <67718947+christianmilfont@users.noreply.github.com>
2025-10-30 16:31:19 -0300

    Fix foreign key relationship in AppDbContext
    Updated the foreign key relationship in the AppDbContext class to reflect changes in the Autor and Livro entities.

--- Context/AppDbContext.cs
+++ Context/AppDbContext.cs
@@ -1,26 +1,26 @@
-﻿using CP6_DotNet.Model;
+using CP6_DotNet.Model;
 using System.Collections.Generic;
 using System.Reflection.Emit;
 using Microsoft.EntityFrameworkCore;
-
+ 
 namespace CP6_DotNet.Context
 {
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-
+ 
         public DbSet<Autor> Autores { get; set; }
         public DbSet<Livro> Livros { get; set; }
-
+ 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-
+ 
             // Configurando o relacionamento
             modelBuilder.Entity<Livro>()
-                .HasOne(l => l.Autor)
+                .HasOne(l => l.Autores)
                 .WithMany(a => a.Livros)
-                .HasForeignKey(l => l.AutorId)
+                .HasForeignKey(l => l.AutoresId)
                 .OnDelete(DeleteBehavior.Cascade); // Quando um autor for deletado, seus livros também serão
         }
     }
